4a59f164070d241cfaa3c1b15cf36642fe21f50d
aminjafer-n3o <amin.jafer@n3o.ltd>
2023-11-01 15:43:43 +0500

    adding the docker assets

---
+++ docker/assets/start-app-and-ssh.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/bash 
+service ssh start
+
+dotnet __PROJECT__.dll
+
+while sleep 15; do
+  ps aux | grep dotnet | grep -q -v grep
+  PROCESS_STATUS=$?
+
+  if [ $PROCESS_STATUS -ne 0 ]; then
+    echo "dotnet process is dead"
+    exit 1
+ fi
+done
