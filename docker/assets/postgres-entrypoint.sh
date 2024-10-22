6392942f6f8ff025eba3ab2572ddf81faf7a73f6
Noor Drummond <noor@ankabut.com>
2024-10-22 15:51:12 +0100

    Update postgres-entrypoint.sh

--- docker/assets/postgres-entrypoint.sh
+++ docker/assets/postgres-entrypoint.sh
@@ -5,7 +5,7 @@ docker-entrypoint.sh postgres -c shared_buffers=256MB -c max_connections=200 &
 sleep 10
 
 echo "archive_mode = on" >> /var/lib/postgresql/data/postgresql.conf
-echo "archive_command = 'pgbackrest --stanza=DataServer archive-push %p'" >> /var/lib/postgresql/data/postgresql.conf
+echo "archive_command = 'pgbackrest --stanza=n3o archive-push %p'" >> /var/lib/postgresql/data/postgresql.conf
 echo "wal_level = replica" >> /var/lib/postgresql/data/postgresql.conf
 echo "max_wal_senders = 3" >> /var/lib/postgresql/data/postgresql.conf
 
