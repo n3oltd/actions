87cf31a96000a84ad2982991c362789bc3927c31
Noor Drummond <noor@ankabut.com>
2024-10-22 15:50:53 +0100

    Update postgres-entrypoint.sh

--- docker/assets/postgres-entrypoint.sh
+++ docker/assets/postgres-entrypoint.sh
@@ -11,9 +11,9 @@ echo "max_wal_senders = 3" >> /var/lib/postgresql/data/postgresql.conf
 
 pg_ctl reload -D /var/lib/postgresql/data
 
-pgbackrest --stanza=DataServer --log-level-console=info stanza-create
+pgbackrest --stanza=n3o --log-level-console=info stanza-create
 
-echo "0 3 * * 0 postgres pgbackrest --stanza=DataServer backup --type=full" >> /etc/crontab
-echo "0 3 * * 1-6 postgres pgbackrest --stanza=DataServer backup --type=diff" >> /etc/crontab
+echo "0 3 * * 0 postgres pgbackrest --stanza=n3o backup --type=full" >> /etc/crontab
+echo "0 3 * * 1-6 postgres pgbackrest --stanza=n3o backup --type=diff" >> /etc/crontab
 
 tail -f /dev/null
