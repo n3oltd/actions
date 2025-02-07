ec7a277c5755674e5f9aec56f0dcd367551676a0
Maria <maria.ahmed@n3o.ltd>
2025-02-07 10:34:45 +0500

    Entity row script added + docker image updated

---
+++ docker/assets/entity-row.sql
@@ -0,0 +1,14 @@
+﻿SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+
+BEGIN TRANSACTION;
+
+CREATE TABLE IF NOT EXISTS public."Entities"(
+                                                "Id" UUID PRIMARY KEY,
+                                                "Revision" INT,
+                                                "DateTime" TIMESTAMP,
+                                                "Type" VARCHAR(400),
+                                                "Json" TEXT
+);
+
+COMMIT TRANSACTION;
+
